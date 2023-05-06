da64473e19cbaa2e0045f1f0b87bdfa936f21e2d
Sidney Aulakh <sidneyaulakh@gmail.com>
2023-05-06 16:59:15 +1000

    session, checkout, cart, images

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php include 'header.php'; ?>
+<?php session_start(); ?>
 <div class="container-fluid pb-3">
     <div class="d-grid gap-3">
-        <div class="bg-body-tertiary border rounded-3">
-            <div class="container">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="main-data">
-                </div>
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="main-data">
             </div>
         </div>
     </div>
