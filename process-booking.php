c38a42de925933cfcf49e417a6112eb1ec28a6ed
Sidney Aulakh <sidneyaulakh@gmail.com>
2023-05-11 12:47:32 +1000

    search and booking

---
+++ process-booking.php
@@ -0,0 +1,24 @@
+<?php
+
+// echo "test";
+
+// print_r($product_array);
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$address2 = $_POST['address2'];
+$country = $_POST['country'];
+$state = $_POST['state'];
+$zip = $_POST['zip'];
+$paymentMethod = $_POST['paymentMethod'];
+$ccName = $_POST['cc-name'];
+$ccNumber = $_POST['cc-number'];
+$ccExpiration = $_POST['cc-expiration'];
+$ccCVV = $_POST['cc-cvv'];
+
+$date = date("l jS \of F Y h:i:s A");
+
+?>
